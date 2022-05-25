2cbd1d25a696f25f6551a8c60215956c9fd5d3fa
hakue <adam.haldas@gmail.com>
2022-05-25 22:44:55 +0200

    change js file

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//Slider
+let counter = 1;
+setInterval(function () {
+    document.getElementById('radio-button-' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 3000)
+//End of Slider
